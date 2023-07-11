1287f0059351cb83a66c1cf6ebfbc6cb8b9aae6b
Dani Rodríguez <dani@danirod.es>
2023-07-11 11:11:38 +0200

    Update the script to support IPv6

--- apply.sh
+++ apply.sh
@@ -3,34 +3,63 @@
 set -e
 cd "$(dirname $0)"
 
-# updates the ipset list, which has the name guardian-ban.
-ipset_apply() {
+# updates the ipset list, which has the name guardian-ban-ipv4
+ipset_apply_ipv4() {
 	generate() {
-		echo "create guardian-ban hash:ip family inet hashsize 65536 maxelem 262144 --exist"
-		echo flush guardian-ban
-		/usr/bin/iprange --print-prefix "add guardian-ban " -1 rules.d/*.list
+		echo "create guardian-ban-ipv4 hash:ip family inet hashsize 65536 maxelem 262144 --exist"
+		echo flush guardian-ban-ipv4
+		/usr/bin/iprange --print-prefix "add guardian-ban-ipv4 " -1 rules-ipv4.d/*.list
 	}
 
-	echo "Reloading pool of IPs..."
+	echo "Reloading pool of IPs for IPv4..."
 	generate | /usr/sbin/ipset restore
-	echo "Pool of IPs successfully read and generated"
-	/usr/sbin/ipset list -terse guardian-ban | grep "Number of entries"
+	echo "Pool of IPs for IPv4 successfully read and generated"
+	/usr/sbin/ipset list -terse guardian-ban-ipv4 | grep "Number of entries"
+}
+
+# updates the ipset list which has the name guardian-ban-ipv6
+ipset_apply_ipv6() {
+	generate() {
+		echo "create guardian-ban-ipv6 hash:ip family inet6 hashsize 65536 maxelem 262144 --exist"
+		echo flush guardian-ban-ipv6
+		# no support for ipv6 in iprange, so we do this manually
+		cat rules-ipv6.d/*.list | sort -h | uniq | sed 's/^/add guardian-ban-ipv6 /'
+	}
+
+	echo "Reloading pool of IPs for IPv6..."
+	generate | /usr/sbin/ipset restore
+	echo "Pool of IPs for IPv6 successfully read and generated"
+	/usr/sbin/ipset list -terse guardian-ban-ipv6 | grep "Number of entries"
 }
 
 # updates the firewall rules if needed.
 iptables_apply() {
-	exist() {
-		echo "Not creating iptables rules because they already exist"
-		echo "(Mistake? Please run iptables -L INPUT and manually review)"
-		echo "Too risky to let the script delete iptables rules on its own"
-		exit 1
+	apply_ipv4() {
+		echo "Applying rules for IPv4"
+		/usr/sbin/iptables -A INPUT -m set --match-set guardian-ban-ipv4 src -j LOG --log-prefix "[guardian-ban-ip] "
+		/usr/sbin/iptables -A INPUT -m set --match-set guardian-ban-ipv4 src -j REJECT
+		echo "Firewall rules for IPv4 have been configured in the INPUT chain"
 	}
 
-	/usr/sbin/iptables -L | grep -q guardian-ban && exist
-	/usr/sbin/iptables -A INPUT -m set --match-set guardian-ban src -j LOG --log-prefix "[guardian-ban-ip] "
-	/usr/sbin/iptables -A INPUT -m set --match-set guardian-ban src -j DROP
-	echo "Firewall rules have been configured in the INPUT chain"
+	apply_ipv6() {
+		echo "Applying rules for IPv6"
+		/usr/sbin/ip6tables -A INPUT -m set --match-set guardian-ban-ipv6 src -j LOG --log-prefix "[guardian-ban-ip] "
+		/usr/sbin/ip6tables -A INPUT -m set --match-set guardian-ban-ipv6 src -j REJECT
+		echo "Firewall rules for IPv6 have been configured in the INPUT chain"
+	}
+
+	skip_ipv4() {
+		echo "Skipping rules for IPv4 because something seems to exist"
+	}
+
+	skip_ipv6() {
+		echo "Skipping rules for IPv6 because something seems to exist"
+	}
+
+	/usr/sbin/iptables -L | grep -q guardian-ban && skip_ipv4 || apply_ipv4
+	/usr/sbin/ip6tables -L | grep -q guardian-ban && skip_ipv6 || apply_ipv6
 }
 
-ipset_apply
+ipset_apply_ipv4
+ipset_apply_ipv6
 iptables_apply
